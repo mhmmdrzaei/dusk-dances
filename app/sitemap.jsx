2bd67060f2363f4721362f96922deef1dfd288f0
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2025-05-03 10:16:43 -0600

    commenting out the sitemap till i figure out the get all slugs

--- app/sitemap.jsx
+++ app/sitemap.jsx
@@ -1,22 +1,22 @@
-import { getAllSlugs } from '../sanity/sanity.utils.js';
+// import { getAllSlugs } from '../sanity/sanity.utils.js';
 
 
-const defualtSiteMapFields = {
-  lastModified: new Date(),
-  changeFrequency: 'monthly',
-  priority: 0.5,
-}
+// const defualtSiteMapFields = {
+//   lastModified: new Date(),
+//   changeFrequency: 'monthly',
+//   priority: 0.5,
+// }
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://duskdance.com';
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://duskdances.com';
 
-export default async function sitemap() {
-  const allPages = await getAllSlugs();
+// export default async function sitemap() {
+//   const allPages = await getAllSlugs();
 
-  return allPages.map((page) => (
-    {
-      ...defualtSiteMapFields,
-      url: `${baseUrl}/${page.url}`,
-      lastModified: page.lastModified,
-    }
-  ));
-}
+//   return allPages.map((page) => (
+//     {
+//       ...defualtSiteMapFields,
+//       url: `${baseUrl}/${page.url}`,
+//       lastModified: page.lastModified,
+//     }
+//   ));
+// }
