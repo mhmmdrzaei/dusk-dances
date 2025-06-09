fdcde90cd3042184d2b488976c16ba50460766b5
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2025-06-09 12:16:22 -0400

    more changes fml

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -7,11 +7,15 @@ const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 export default function VideoPlayer({ value }) {
   console.log(value);
   return (
-    <div className="videoPlayer">
+    <div className="videoPlayerContainer">
+          <div className="videoPlayer">
           <ReactPlayer url={value.url} width="100%"
         height="100%"/>
 
     </div>
+
+    </div>
+
 
   );
 }
